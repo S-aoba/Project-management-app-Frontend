aae2e6d518dea8794037f50464fcf699b5b93d19
aoba <writer_fairy_road@yahoo.co.jp>
2024-09-10 22:12:45 +0900

    ✨ Add fetch userProjects function.

---
+++ src/features/project/api/use-projects.ts
@@ -0,0 +1,40 @@
+import { useQuery } from '@tanstack/react-query'
+
+import { useCsrfToken } from '@/features/auth/api/use-csrf-token'
+
+import { Project } from '@/types/type'
+
+type ResponseType = {
+  data: Project[]
+}
+
+export const useProjects = () => {
+  const { csrfToken, getCsrfToken } = useCsrfToken()
+
+  const { data } = useQuery<ResponseType>({
+    queryKey: ['userProjects'],
+    queryFn: async () => {
+      await csrfToken()
+
+      const csrf = getCsrfToken()
+
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/api/user/projects`, {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          'X-XSRF-TOKEN': csrf!,
+        },
+      })
+
+      if (!res.ok) {
+        throw new Error('Unauthenticated.')
+      }
+
+      return res.json()
+    },
+  })
+
+  return { data }
+}
