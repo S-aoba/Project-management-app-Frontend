66b246d3880d135d6bcc2b643b6c5e9122f82e63
aoba <writer_fairy_road@yahoo.co.jp>
2024-09-13 19:19:10 +0900

    🎨 change: modalState have two states.

--- src/features/task/store/use-edit-task-sheet.ts
+++ src/features/task/store/use-edit-task-sheet.ts
@@ -1,6 +1,14 @@
 import { atom, useAtom } from 'jotai'
 
-const modalState = atom(false)
+type OpenTaskState = {
+  isOpen: boolean,
+  id: number | undefined
+}
+
+const modalState = atom<OpenTaskState>({
+  isOpen: false,
+  id: undefined,
+})
 
 export const useEditTaskSheet = () => {
   return useAtom(modalState)
