14340cf6a0f5464a1576b8c60ab9ec49f4df7b10
aoba <writer_fairy_road@yahoo.co.jp>
2024-09-16 16:39:34 +0900

    💄 style: fix create an account word ui. (#30)

--- src/features/auth/components/login-form.tsx
+++ src/features/auth/components/login-form.tsx
@@ -35,39 +35,46 @@ export const LoginForm = () => {
   }
 
   return (
-    <Card className='w-full h-full p-8'>
-      {error && <InputError message={error} />}
-      <CardHeader className='px-0 pt-0'>
-        <CardTitle>Login</CardTitle>
-        <CardDescription>Use your email.</CardDescription>
-      </CardHeader>
-      <CardContent className='space-y-5 px-0 pb-0'>
-        <form className='space-y-2.5' onSubmit={handleSubmit}>
-          <Input
-            autoFocus
-            disabled={isLoginPending}
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder='Email'
-            type='email'
-            required
-          />
-          <Input
-            disabled={isLoginPending}
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder='Password'
-            type='password'
-            required
-          />
-          <Button type='submit' className='w-full' size='lg' disabled={isLoginPending}>
-            Login
-          </Button>
-        </form>
-        <div className='pt-2 text-xs text-muted-foreground hover:underline hover:underline-offset-2 flex justify-end'>
-          <Link href={'#'}>No account yet ?</Link>
-        </div>
-      </CardContent>
-    </Card>
+    <>
+      <Card className='w-full h-full p-8'>
+        {error && <InputError message={error} />}
+        <CardHeader className='px-0 pt-0'>
+          <CardTitle>Login</CardTitle>
+          <CardDescription>Use your email.</CardDescription>
+        </CardHeader>
+        <CardContent className='space-y-5 px-0 pb-0'>
+          <form className='space-y-2.5' onSubmit={handleSubmit}>
+            <Input
+              autoFocus
+              disabled={isLoginPending}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder='Email'
+              type='email'
+              required
+            />
+            <Input
+              disabled={isLoginPending}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder='Password'
+              type='password'
+              required
+            />
+            <Button type='submit' className='w-full' size='lg' disabled={isLoginPending}>
+              Login
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+      <div className='p-4 text-end'>
+        <p className='text-sm text-foreground'>
+          Don’t have an account? {''}
+          <Link href={'#'} className='text-blue-500 hover:underline hover:underline-offset-2'>
+            Create an account
+          </Link>
+        </p>
+      </div>
+    </>
   )
 }
