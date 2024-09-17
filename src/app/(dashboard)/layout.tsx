4f8e7d7f3bf360e538f97d20ee57e8b142e4dc1f
aoba <writer_fairy_road@yahoo.co.jp>
2024-09-17 10:54:06 +0900

    💄 style: fix navigation ui. (#30)

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,4 @@
-import { Navigation } from "@/components/navigation"
+import { Navigation } from '@/components/navigation'
 
 export default function DashboardLayout({
   children,
@@ -6,7 +6,7 @@ export default function DashboardLayout({
   children: React.ReactNode
 }>) {
   return (
-    <div className='h-full w-full flex'>
+    <div className='h-full w-full flex space-x-4 p-8 bg-blue-600/40'>
       <Navigation />
       {children}
     </div>
