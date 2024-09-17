77be36ad536f3f1bd98129c4a488c2e805260346
aoba <writer_fairy_road@yahoo.co.jp>
2024-09-17 22:24:12 +0900

    💄 style: add status icon. (#30)

--- src/components/status.tsx
+++ src/components/status.tsx
@@ -3,10 +3,20 @@ import { Badge } from '@/components/ui/badge'
 import { getStatusVariant } from '@/lib/colors'
 
 import { Task } from '@/types/type'
+import { CircleCheck, CirclePause, Timer } from 'lucide-react'
 
 type Props = Pick<Task, 'status'>
 
 export const Status = ({ status }: Props) => {
   const color = getStatusVariant({ status })
-  return <Badge variant={color}>{status}</Badge>
+  return (
+    <div className='flex items-center'>
+      <Badge variant={color}>
+        {status === 'pending' && <CirclePause className='size-4 mr-2' />}
+        {status === 'is_progress' && <Timer className='size-4 mr-2' />}
+        {status === 'completed' && <CircleCheck className='size-4 mr-2' />}
+        {status}
+      </Badge>
+    </div>
+  )
 }
