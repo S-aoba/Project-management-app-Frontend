b18ed383daa68548748fd428d13d37e8292020d7
aoba <writer_fairy_road@yahoo.co.jp>
2024-09-18 21:10:17 +0900

    💄 style: fix user list. (#30)

--- src/app/(dashboard)/projects/[projectId]/page.tsx
+++ src/app/(dashboard)/projects/[projectId]/page.tsx
@@ -1,13 +1,12 @@
 // import { UserList } from '@/components/user-list'
+import { UserList } from '@/components/user-list'
 import { ProjectInfomation } from '@/features/project/components/project-information'
 
 export default function ProjectDetailPage() {
   return (
     <div className='h-full w-full grid grid-cols-12 gap-x-4'>
       <ProjectInfomation />
-      {/* <UserList /> */}
-
-      <div className='col-span-1 p-8'>UserList</div>
+      <UserList />
     </div>
   )
 }
--- src/components/user-list.tsx
+++ src/components/user-list.tsx
@@ -16,15 +16,15 @@ export const UserList = () => {
   const { data, isPending } = useProject(projectId)
 
   // const [ConfirDialog, confirm] = useConfirm('Are you sure?', 'You are about to perform a delete action.')
-  const onDelete = async (id: number) => {
-    const ok = await confirm()
-    return
-  }
+  // const onDelete = async (id: number) => {
+  //   const ok = await confirm()
+  //   return
+  // }
 
   return (
-    <div className='h-full flex flex-col space-y-4 py-8 px-2 w-40 border-l'>
+    <div className='h-full flex flex-col space-y-4 py-8 px-2 w-40'>
       {/* <ConfirDialog /> */}
-      <h1 className='text-muted-foreground text-sm'>Participating User List</h1>
+      <h1 className='text-muted-foreground text-sm'>Member</h1>
       {isPending ? (
         <div className='h-full p-2 space-y-4'>
           <div className='flex w-full space-x-2'>
@@ -41,7 +41,7 @@ export const UserList = () => {
           </div>
         </div>
       ) : (
-        data?.users.map((user) => <UserItem key={user.id} id={user.id} name={user.name} onDelete={onDelete} />)
+        data?.users.map((user) => <UserItem key={user.id} id={user.id} name={user.name} onDelete={() => {}} />)
       )}
     </div>
   )
