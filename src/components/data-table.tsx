c2aa9154d25d53a118751d42a59ab0f5068e3064
aoba <writer_fairy_road@yahoo.co.jp>
2024-09-19 10:18:48 +0900

    👔 🚧 feat: add filtering of task name function. (#32)

--- src/components/data-table.tsx
+++ src/components/data-table.tsx
@@ -4,22 +4,27 @@ import * as React from 'react'
 
 import {
   ColumnDef,
+  ColumnFiltersState,
   SortingState,
   flexRender,
   getCoreRowModel,
+  getFilteredRowModel,
   getSortedRowModel,
   useReactTable,
 } from '@tanstack/react-table'
 
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
+import { Input } from './ui/input'
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
   data: TData[]
+  filterKey: string
 }
 
-export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData, TValue>) {
+export function DataTable<TData, TValue>({ columns, data, filterKey }: DataTableProps<TData, TValue>) {
   const [sorting, setSorting] = React.useState<SortingState>([])
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([])
   const [rowSelection, setRowSelection] = React.useState({})
 
   const table = useReactTable({
@@ -29,14 +34,25 @@ export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData
     onRowSelectionChange: setRowSelection,
     onSortingChange: setSorting,
     getSortedRowModel: getSortedRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
     state: {
       rowSelection,
       sorting,
+      columnFilters,
     },
   })
 
   return (
     <div>
+      <div className='flex items-center py-4'>
+        <Input
+          placeholder={`Filter ${filterKey}...`}
+          value={(table.getColumn(filterKey)?.getFilterValue() as string) ?? ''}
+          onChange={(event) => table.getColumn(filterKey)?.setFilterValue(event.target.value)}
+          className='max-w-sm'
+        />
+      </div>
       <div className='rounded-md border'>
         <Table>
           <TableHeader>
