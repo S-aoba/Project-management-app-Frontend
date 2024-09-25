4693b3a4e319d2cab6ca747f0f9dfd193e4cbb97
S-aoba <writer_fairy_road@yahoo.co.jp>
2024-09-25 10:56:21 +0900

    feat: add generate invite modal in modals. (#25)

--- src/components/modals.tsx
+++ src/components/modals.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react'
 
 import { CreateProjectModal } from '@/features/project/components/create-project-modal'
 import { CreateTaskModal } from '@/features/task/components/create-task-modal'
+import { GenerateInviteCodeModal } from '@/features/project/components/generate-invite-code-modal'
 
 export const Modals = () => {
   const [mounted, setMounted] = useState<boolean>(false)
@@ -19,6 +20,8 @@ export const Modals = () => {
       <CreateProjectModal />
 
       <CreateTaskModal />
+
+      <GenerateInviteCodeModal />
     </>
   )
 }
