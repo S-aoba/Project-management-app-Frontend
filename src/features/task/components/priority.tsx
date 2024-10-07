63acb86a0087aa0fc6f88be77ce30c5a2cfc987f
S-aoba <writer_fairy_road@yahoo.co.jp>
2024-10-07 15:55:48 +0900

    change: icon color

--- src/features/task/components/priority.tsx
+++ src/features/task/components/priority.tsx
@@ -4,9 +4,9 @@ import { ArrowDown, ArrowRight, ArrowUp } from 'lucide-react'
 export const Priority = ({ priority }: Pick<Task, 'priority'>) => {
   return (
     <div className='flex items-center'>
-      {priority === 'low' && <ArrowDown className='size-4 text-gray-800/60' />}
-      {priority === 'medium' && <ArrowRight className='size-4 text-gray-800/60' />}
-      {priority === 'high' && <ArrowUp className='size-4 text-gray-800/60' />}
+      {priority === 'low' && <ArrowDown className='size-4' />}
+      {priority === 'medium' && <ArrowRight className='size-4' />}
+      {priority === 'high' && <ArrowUp className='size-4' />}
       <span className='text-sm ml-1 capitalize'>{priority}</span>
     </div>
   )
